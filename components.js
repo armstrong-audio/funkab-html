d8159b3950605ef021943b094e6ec8e4e8b044ed
Kaspar L. Palgi <git@e-stonia.co.uk>
2024-11-05 16:36:05 +0200

    feat: separate components working

--- components.js
+++ components.js
@@ -4,5 +4,12 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(data => {
             document.getElementById('contactbar-container').innerHTML = data;
         })
-        .catch(error => console.error('Error loading preloader:', error));
+        .catch(error => console.error('Error loading contact bar:', error));
+
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer-container').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading footer:', error));
 });
